023ad3f4c22c84be01971f4f625461f79ed2b9b3
Isaac <isaaafc@gmail.com>
2017-04-07 00:01:32 +0800

    heroku 2

--- mysongsdb/settings.py
+++ mysongsdb/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -118,8 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT='/home/isaac/Documents/Projects/songsdb/static'
+STATIC_ROOT=os.path.join(BASE_DIR, 'songsdb', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    '/home/isaac/Documents/Projects/songsdb/static-assets',
+    os.path.join(BASE_DIR, 'songsdb', 'static-assets'),
 )
