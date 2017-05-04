5796ac96bc46ec1149f14e9259b62a9786ce5cbc
Isaac <isaaafc@gmail.com>
2017-05-04 22:57:02 +0800

    changed

--- songsdb/forms.py
+++ songsdb/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from .models import Author, Publisher, Type
 
 class AddForm(forms.Form):
     song_name = forms.CharField(label='Song name', max_length=50)
     document_link = forms.CharField(label='Document link', max_length=200)
     year = forms.IntegerField(label='Published year', max_value=2100, min_value=1800)
     author = forms.CharField(label='Author', max_length=50)
+    author_choice = forms.ModelChoiceField(label='Existing Authors', queryset=Author.objects.all())
     publisher = forms.CharField(label='Publisher', max_length=50)
     song_type = forms.CharField(label='Song type', max_length=50)
 
