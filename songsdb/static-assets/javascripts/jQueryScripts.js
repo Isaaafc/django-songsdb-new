8144b6b4c266a1c6fd3bd06cc87634927fe882cd
Isaac <isaaafc@gmail.com>
2017-05-15 00:19:41 +0800

    ad

--- songsdb/static-assets/javascripts/jQueryScripts.js
+++ songsdb/static-assets/javascripts/jQueryScripts.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
     $(".toggleAuthor").click(function(){
         $("#field_id_author").toggle();
+	$("#field_id_author").val('');
+	$("#field_id_author_choice").val('');
 	$("#field_id_author_choice").toggle();
     });
     $(".togglePublisher").click(function(){
         $("#field_id_publisher").toggle();
 	$("#field_id_publisher_choice").toggle();
+        $("#field_id_publisher").val('');
+	$("#field_id_publisher_choice").val('');
     });
     $(".toggleType").click(function(){
         $("#field_id_song_type").toggle();
 	$("#field_id_type_choice").toggle();
+        $("#field_id_song_type").val('');
+	$("#field_id_type_choice").val('');
     });    
 });
 
