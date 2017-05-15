ab14c7878c47e40b216e00a12e8c299f0eeaca8e
Isaac <isaaafc@gmail.com>
2017-05-15 23:38:06 +0800

    sdf

--- songsdb/forms.py
+++ songsdb/forms.py
@@ -6,11 +6,11 @@ class AddForm(forms.Form):
     document_link = forms.CharField(label='Document link', max_length=200)
     year = forms.IntegerField(label='Published year', max_value=2100, min_value=1800)
     author = forms.CharField(label='Author', max_length=50)
-    author_choice = forms.ModelChoiceField(label='Existing Authors', queryset=Author.objects.all(), required=False)
+    author_choice = forms.ModelChoiceField(label='Author', queryset=Author.objects.all(), required=False)
     publisher = forms.CharField(label='Publisher', max_length=50)
-    publisher_choice = forms.ModelChoiceField(label='Existing Publishers', queryset=Publisher.objects.all(), required=False)
+    publisher_choice = forms.ModelChoiceField(label='Publisher', queryset=Publisher.objects.all(), required=False)
     song_type = forms.CharField(label='Song type', max_length=50)
-    type_choice=forms.ModelChoiceField(label='Existing Types', queryset=Type.objects.all(), required=False)
+    type_choice=forms.ModelChoiceField(label='Song type', queryset=Type.objects.all(), required=False)
 
 class SearchForm(forms.Form):
     search_bar = forms.CharField(label='Enter keyword', max_length=200)
