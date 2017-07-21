c826f0f9df7f96161b622bcd6e138c247d0d68c9
Isaac <isaaafc@gmail.com>
2017-07-22 01:27:10 +0800

    fkfkfk

--- mysongsdb/settings.py
+++ mysongsdb/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'songsdb', 'static-assets'),
 )
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
