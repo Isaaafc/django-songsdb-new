eea661c7e01c6b25a49fccf092335ea1c0653e7d
Isaac <isaaafc@gmail.com>
2017-08-23 12:06:14 +0800

    bug fix 5

--- songsdb/views.py
+++ songsdb/views.py
@@ -126,10 +126,12 @@ def edit_song(request):
             song_link = form.cleaned_data['document_link']
             #song_year = form.cleaned_data['year']
             song_year = "1870"
-            author = form.cleaned_data['author']
-            author_choice = form.cleaned_data['author_choice']
+            #author = form.cleaned_data['author']
+            #author_choice = form.cleaned_data['author_choice']
             #publisher = form.cleaned_data['publisher']
             #publisher_choice = form.cleaned_data['publisher_choice']
+            author = None
+            author_choice = None            
             publisher = None
             publisher_choice = None
             song_type = form.cleaned_data['song_type']
