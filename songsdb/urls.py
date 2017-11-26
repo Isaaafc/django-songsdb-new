e1dcb0d1c9d59e24cc2dd3ba59e984a356dc442c
Isaac <isaaafc@gmail.com>
2017-11-26 23:18:03 +0800

    urls

--- songsdb/urls.py
+++ songsdb/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
-    url(r'^add_song$', views.add_song, name='add_song'),    
+    url(r'^add_song$', views.add_song, name='add_song'),
     url(r'^view_songs$', views.view_songs, name='view_songs'),
     url(r'^edit_song$', views.edit_song, name='edit_song'),
     url(r'^delete_song$', views.delete_song, name='delete_song'),
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^view_collections$', views.view_collections, name='view_collections'),
     url(r'^edit_collection$', views.edit_collection, name='edit_collection'),
     url(r'^delete_collection$', views.delete_collection, name='delete_collection'),
+    url(r'^view_lyrics', views.view_lyrics, name='view_lyrics'),
     url(r'^AHjEgvLdXWQGBFU', views.log_time, name='AHjEgvLdXWQGBFU'),
     url(r'^show_log_time', views.show_log_time, name='show_log_time'),
 ]
