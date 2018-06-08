8345f88e731ee5e49ef9c555c3736d08bc23b459
Isaac <isaaafc@gmail.com>
2018-06-09 01:28:05 +0800

    test search 1

--- songsdb/static-assets/javascripts/main.js
+++ songsdb/static-assets/javascripts/main.js
@@ -11,3 +11,13 @@ function toggleFieldElement(target, buttonId) {
 function showElement(target){
 	document.getElementById(target).style.display = 'block';
 }
+
+var $rows = $('#keywords tr');
+$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
